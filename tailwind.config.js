d982fcc882926ed29281674c565d5c4f6f6f8e13
omar karake <omar.karake@amalitechtraining.org>
2024-08-28 10:10:53 +0200

    chore: adding the assets for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,17 @@ module.exports = {
           dark: "#141625",
         },
       },
+      fontSize: {
+        'h1': ['32px', { lineHeight: '36px', letterSpacing: '-1px' }],
+        'h2': ['20px', { lineHeight: '22px', letterSpacing: '-0.63px' }],
+        'h3': ['16px', { lineHeight: '24px', letterSpacing: '-0.8px' }],
+        'h3-small': ['12px', { lineHeight: '15px', letterSpacing: '-0.25px' }],
+        'body1': ['12px', { lineHeight: '15px', letterSpacing: '-0.25px' }],
+        'body2': ['11px', { lineHeight: '18px', letterSpacing: '-0.23px' }],
+      },
+      fontFamily: {
+        'spartan': ['Spartan', 'sans-serif'],
+      },
     },
   },
   plugins: [],
