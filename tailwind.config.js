72a22029afeef7073726cd3f3d9cc94740f87466
omar karake <omar.karake@amalitechtraining.org>
2024-08-28 09:51:23 +0200

    chore: adding tailwind configuration colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#7C5DFA",
+          light: "#9277FF",
+        },
+        secondary: {
+          dark: "#1E2139", 
+          darker: "#252945", 
+        },
+        accent: {
+          light: "#DFE3FA", 
+          muted: "#888EB0", 
+          medium: "#7E88C3", 
+          dark: "#0C0E16", 
+        },
+        danger: {
+          DEFAULT: "#EC5757",
+          light: "#FF9797",
+        },
+        background: {
+          light: "#F8F8FB",
+          dark: "#141625",
+        },
+      },
+    },
   },
   plugins: [],
 };
