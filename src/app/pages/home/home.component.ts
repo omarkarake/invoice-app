ac33d56dd0a8d19b34ba8d8f3821a928f535f158
omar karake <omar.karake@amalitechtraining.org>
2024-08-29 14:15:17 +0200

    chore: adding slider from left to it's space that will be used to create invoice

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state(
+        'in',
+        style({
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'out',
+        style({
+          transform: 'translateX(-100%)',
+        })
+      ),
+      transition('out => in', [animate('300ms ease-in')]),
+      transition('in => out', [animate('300ms ease-out')]),
+    ]),
+  ],
 })
 export class HomeComponent {
+  invoiceCreateSlide: boolean = false;
 
+  constructor() {}
+
+  newInvoiceTrigger() {
+    this.invoiceCreateSlide = !this.invoiceCreateSlide;
+  }
+
+  closeInvoice() {
+    this.invoiceCreateSlide = false;
+  }
 }
