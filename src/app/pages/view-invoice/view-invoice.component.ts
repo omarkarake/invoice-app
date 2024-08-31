9790373fad6e0c326cf209a4496581ed43106099
omar karake <omar.karake@amalitechtraining.org>
2024-08-31 16:38:51 +0200

    creating a formBuilder with formGroup and formControl for initial invoice data, because I am about to edit

--- src/app/pages/view-invoice/view-invoice.component.ts
+++ src/app/pages/view-invoice/view-invoice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {
   trigger,
@@ -9,14 +9,25 @@ import {
 } from '@angular/animations';
 import { Store } from '@ngrx/store';
 import { selectInvoiceById } from '../../store/selectors/invoice.selector';
-import { Invoice } from '../../models/invoice.model';
+import { Invoice, Item } from '../../models/invoice.model';
 import { Observable, startWith } from 'rxjs';
-import { deleteInvoice, updateInvoiceStatus } from '../../store/actions/invoice.action';
+import {
+  deleteInvoice,
+  updateInvoiceStatus,
+} from '../../store/actions/invoice.action';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+} from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-view-invoice',
   templateUrl: './view-invoice.component.html',
-  styleUrl: './view-invoice.component.css',
+  styleUrls: ['./view-invoice.component.css'],
   animations: [
     trigger('slideInOut', [
       state(
@@ -35,19 +46,23 @@ import { deleteInvoice, updateInvoiceStatus } from '../../store/actions/invoice.
       transition('in => out', [animate('300ms ease-out')]),
     ]),
   ],
+  providers: [DatePipe],
 })
-export class ViewInvoiceComponent implements OnInit {
+export class ViewInvoiceComponent implements OnInit, DoCheck {
   invoiceEditSlide: boolean = true;
   isModalOpen = false;
   invoiceId: string | null = null;
   invoice$: Observable<Invoice | null | undefined> | null = null;
+  invoiceForm!: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private store: Store<{
       appState: { invoice: Invoice[]; filteredInvoice: string[] };
     }>,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder,
+    private datePipe: DatePipe
   ) {}
 
   ngOnInit() {
@@ -56,22 +71,121 @@ export class ViewInvoiceComponent implements OnInit {
       if (this.invoiceId) {
         this.invoice$ = this.store
           .select(selectInvoiceById(this.invoiceId))
-          .pipe(startWith(null)); // This ensures the observable starts with null
+          .pipe(startWith(null));
+
+        this.invoice$.subscribe((invoice) => {
+          if (invoice) {
+            this.initializeForm(invoice);
+          }
+        });
       }
     });
   }
 
+  ngDoCheck(): void {
+    console.log('formData: ', this.invoiceForm.value);
+  }
+
+  initializeForm(invoice: Invoice) {
+    this.invoiceForm = this.fb.group({
+      streetAddress: [invoice.senderAddress.street, Validators.required],
+      city: [invoice.senderAddress.city, Validators.required],
+      postCode: [invoice.senderAddress.postCode, Validators.required],
+      country: [invoice.senderAddress.country, Validators.required],
+      clientName: [invoice.clientName, Validators.required],
+      clientEmail: [invoice.clientEmail, Validators.required],
+      ClientstreetAddress: [invoice.clientAddress.street, Validators.required],
+      clientCity: [invoice.clientAddress.city, Validators.required],
+      clientPostCode: [invoice.clientAddress.postCode, Validators.required],
+      clientCountry: [invoice.clientAddress.country, Validators.required],
+      InvoiceDate: [invoice.createdAt, Validators.required],
+      paymentTerms: [invoice.paymentTerms, Validators.required],
+      projectDescription: [invoice.description, Validators.required],
+      items: this.fb.array(invoice.items.map((item) => this.createItem(item))),
+      status: [invoice.status],
+    });
+  }
+
+  createItem(item?: Item): FormGroup {
+    return this.fb.group({
+      itemName: [item ? item.name : '', Validators.required],
+      quantity: [
+        item ? item.quantity : 0,
+        [Validators.required, Validators.min(1)],
+      ],
+      price: [item ? item.price : 0, [Validators.required, Validators.min(0)]],
+      total: [{ value: item ? item.total : 0 }],
+    });
+  }
+
+  get streetAddressControl(): FormControl {
+    return this.invoiceForm.get('streetAddress') as FormControl;
+  }
+
+  get cityControl(): FormControl {
+    return this.invoiceForm.get('city') as FormControl;
+  }
+
+  get postCodeControl(): FormControl {
+    return this.invoiceForm.get('postCode') as FormControl;
+  }
+
+  get countryControl(): FormControl {
+    return this.invoiceForm.get('country') as FormControl;
+  }
+
+  get clientNameControl(): FormControl {
+    return this.invoiceForm.get('clientName') as FormControl;
+  }
+
+  get clientEmailControl(): FormControl {
+    return this.invoiceForm.get('clientEmail') as FormControl;
+  }
+
+  get clientStreetAdressControl(): FormControl {
+    return this.invoiceForm.get('ClientstreetAddress') as FormControl;
+  }
+
+  get clientCityControl(): FormControl {
+    return this.invoiceForm.get('clientCity') as FormControl;
+  }
+
+  get clientPostCodeControl(): FormControl {
+    return this.invoiceForm.get('clientPostCode') as FormControl;
+  }
+
+  get clientCountryControl(): FormControl {
+    return this.invoiceForm.get('clientCountry') as FormControl;
+  }
+
+  get invoiceDateControl(): FormControl {
+    return this.invoiceForm.get('InvoiceDate') as FormControl;
+  }
+
+  get paymentTermsControl(): FormControl {
+    return this.invoiceForm.get('paymentTerms') as FormControl;
+  }
+
+  get projectDescriptionControl(): FormControl {
+    return this.invoiceForm.get('projectDescription') as FormControl;
+  }
+
+  get items(): FormArray {
+    return this.invoiceForm.get('items') as FormArray;
+  }
+
   editInvoiceTrigger() {
-    // this.invoiceEditSlide = !this.invoiceEditSlide;
+    this.invoiceEditSlide = !this.invoiceEditSlide;
   }
 
   closeInvoice() {
-    // this.invoiceEditSlide = false;
+    this.invoiceEditSlide = false;
   }
 
   markAsPaid() {
-    this.invoiceId &&
+    if (this.invoiceId) {
       this.store.dispatch(updateInvoiceStatus({ id: this.invoiceId }));
+    }
   }
 
   openModal() {
@@ -96,12 +210,10 @@ export class ViewInvoiceComponent implements OnInit {
 
   deleteInvoice() {
     console.log('deleteInvoice with id: ', this.invoiceId);
-    this.invoiceId &&
-    this.store.dispatch(deleteInvoice({ id: this.invoiceId }));
-    this.closeModal();
-    this.router.navigate(['/home']);
+    if (this.invoiceId) {
+      this.store.dispatch(deleteInvoice({ id: this.invoiceId }));
+      this.closeModal();
+      this.router.navigate(['/home']);
+    }
   }
-}
-function markInvoiceAsPaid(arg0: { id: string | null }): any {
-  throw new Error('Function not implemented.');
 }
